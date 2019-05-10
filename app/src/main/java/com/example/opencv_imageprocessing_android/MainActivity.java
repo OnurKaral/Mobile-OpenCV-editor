5c9f274559e648861c62ba7290ab7ab0a7ee15a2
qwertyF0 <santobixua@gmail.com>
2019-05-10 19:09:49 +0300

    Login-Register System with Firebase Database

--- app/src/main/java/com/example/opencv_imageprocessing_android/MainActivity.java
+++ app/src/main/java/com/example/opencv_imageprocessing_android/MainActivity.java
@@ -1,122 +1,119 @@
 package com.example.opencv_imageprocessing_android;
+
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
-
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
-
-    private static  final String TAG = MainActivity.class.getSimpleName();
-
-    private JavaCameraView cameraView;
-
-    private Mat source;
-
-
-    private BaseLoaderCallback openCVLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-
-            switch (status) {
-
-                case LoaderCallbackInterface.SUCCESS:
-
-                    Log.i(TAG, "OpenCV loaded successfully");
-                    cameraView.enableView();
-                    cameraView.setVisibility(View.VISIBLE);
-                    break;
-
-                default:
-                    super.onManagerConnected(status);
-                    break;
-            }
-        }
-    };
-
-
-
+public class MainActivity extends AppCompatActivity {
+    //XML bileşen tanımlama
+    private EditText editTextUserName;
+    private EditText editTextUserPassword;
+    private Button buttonLogin;
+    private TextView txtRegister;
+    private FirebaseAuth mAuth;
+    private FirebaseUser firebaseUser;
+    private String userName;
+    private String userPassword;
+    AnimationDrawable animationDrawable;
+    RelativeLayout relativeLayout;
+    //****************************************************************************************************
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cameraView = findViewById(R.id.camera);
+        editTextUserName = (EditText)findViewById(R.id.editTextUserName);
+        editTextUserPassword = (EditText)findViewById(R.id.editTextUserPassword);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        txtRegister = (TextView) findViewById(R.id.txtRegister);
+        relativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout);
 
-        cameraView.setVisibility(View.VISIBLE);
-        cameraView.setCvCameraViewListener(this);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();
+
+        if(firebaseUser != null){
+
+            Intent i = new Intent(MainActivity.this,ProfileActivity.class);
+            startActivity(i);
+            finish();
+        }
+        //Login İşlemi
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                userName = editTextUserName.getText().toString();
+                userPassword = editTextUserPassword.getText().toString();
+                if(userName.isEmpty() || userPassword.isEmpty()){
+
+                    Toast.makeText(getApplicationContext(),"Lütfen gerekli alanları doldurunuz!",Toast.LENGTH_SHORT).show();
+
+                }else{
+
+                    loginFunc();
+                }
+            }
+        });
+
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning())
+            animationDrawable.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-        if (cameraView != null) {
-            cameraView.disableView();
-        }
+        if (animationDrawable != null && animationDrawable.isRunning())
+            animationDrawable.stop();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (!OpenCVLoader.initDebug()) {
+    private void loginFunc() {
 
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, this, openCVLoaderCallback);
+        mAuth.signInWithEmailAndPassword(userName,userPassword).addOnCompleteListener(MainActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
 
-        } else {
+                            Intent i = new Intent(MainActivity.this,ProfileActivity.class);
+                            startActivity(i);
+                            finish();
 
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-            openCVLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-    }
+                        }
+                        else{
+                            // hata
+                            Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
-
-
-    @Override
-    protected void onDestroy() {
-
-        if (cameraView != null) {
-            cameraView.disableView();
-        }
-
-        super.onDestroy();
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-
-        source = new Mat(height, width, CvType.CV_8UC4);
-
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-
-        source.release();
-
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-
-        source = inputFrame.rgba();
-
-        Imgproc.cvtColor(source, source, Imgproc.COLOR_BGR2GRAY);
-        Imgproc.blur(source, source, new Size(5., 5.));
-        Imgproc.Canny(source, source, 100, 20);
-
-        return source;
+                });
     }
 }
+
